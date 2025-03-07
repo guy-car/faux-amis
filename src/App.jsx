0e66a0ab5f87f1c4405abe24ee761b8c365a00e7
guy-car <guil.crn@gmail.com>
2025-03-07 16:24:43 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>I'm a Vite / React / JS template</h1>
+      <img src="./public/guy-car.png"></img>
+    </>
+  )
+}
+
+export default App
